1540bfe42e2a8150951ee63c929e835147ebf6b5
manuelbamise <etobi1930@gmail.com>
2025-05-06 15:16:37 +0100

    process: installed tailwindcss through the terminal

--- frontendFolder/electron.vite.config.ts
+++ frontendFolder/electron.vite.config.ts
@@ -1,10 +1,13 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin(), tailwindcss()]
   },
   preload: {
     plugins: [externalizeDepsPlugin()]

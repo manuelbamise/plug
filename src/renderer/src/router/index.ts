4433b782a5dcb7405f68a99b5486d06feed027f3
manuelbamise <etobi1930@gmail.com>
2025-05-23 16:04:59 +0100

    process: routine save

--- src/renderer/src/router/index.ts
+++ src/renderer/src/router/index.ts
@@ -1,1 +1,17 @@
-import { createRouter } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '../pages/Home.vue'
+import Settings from '../pages/Settings.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+
+  { path: '/settings', name: 'settings', component: Settings }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
